a54205d8066df173da895bcb8d1d4ced3249aa91
Jacob Baum <161981442+Jacobbaum19@users.noreply.github.com>
2024-04-20 11:26:21 -0600

    Update main.py

--- flight_price_tracker_project/main.py
+++ flight_price_tracker_project/main.py
@@ -12,12 +12,14 @@ if sheet_data[0]["iataCode"] == "":
         Price is feteched with the new Iatacodes and new_price and new_link is created from them. 
         Checks to see if new_price is not empty and the new price is lower than the existing price on the
         sheet."""
+        
         city_name = row["city"]
         iata_code = dm.get_iata_code(city_name)
         row["iataCode"] = iata_code
         dm.update_sheet(row, iata_code)
         print(f"Fetching price for {city_name} with IATA code {iata_code}")
         new_price, new_link = flight_search.get_price_for_flight(city_name, iata_code)
+        
         if new_price and new_price < row["lowestPrice"]:
             dm.update_sheet_price_url(row, new_price, new_link)
             print(f"Lower price found for {city_name}: €{new_price}")
